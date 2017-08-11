2642b24e98934ebb4634c83df688c4db93b1984b
Austin Chiang <austinscchiang@gmail.com>
2017-08-11 19:54:45 +0800

    [util] Modularize utils for easier testing

--- util/bracket_points.py
+++ util/bracket_points.py
@@ -9,23 +9,32 @@ from __future__ import print_function
 
 INPUT_FILE = "data.txt"
 OUTPUT_FILE = "placing_rewards.txt"
-place_to_points = {}
 
-with open(INPUT_FILE, "rb") as f:
-    for line in f:
-        tokens = line.split()
-        if (len(tokens) == 2 and
-            tokens[0].isdigit() and
-            tokens[1].isdigit()):
-            place = int(tokens[0])
-            points = int(tokens[1])
-            if (place in place_to_points and
-                not place_to_points[place] == points):
-                raise ValueError("""Place does not always correspond to same
-                                 point value""")
+def placings_read(input_file=INPUT_FILE):
+    place_to_points = {}
+    with open(input_file, "rb") as f:
+        for line in f:
+            tokens = line.split()
+            if (len(tokens) == 2 and
+                tokens[0].isdigit() and
+                tokens[1].isdigit()):
+                place = int(tokens[0])
+                points = int(tokens[1])
+                if (place in place_to_points and
+                    not place_to_points[place] == points):
+                    raise ValueError("""Place does not always correspond to same
+                                     point value""")
 
-            place_to_points[place] = points
+                place_to_points[place] = points
 
-with open (OUTPUT_FILE, "wb") as w:
-    for key in sorted(place_to_points):
-        print("{} {}".format(key, place_to_points[key]), file=w)
+    return place_to_points
+
+def placings_write(mapping, output_file=OUTPUT_FILE):
+    with open (output_file, "wb") as w:
+        for key in sorted(mapping):
+            print("{} {}".format(key, mapping[key]), file=w)
+
+
+if __name__ == "__main__":
+    place_to_points = placings_read()
+    placings_write(place_to_points)
