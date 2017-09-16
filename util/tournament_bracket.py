23fe4d622e3c61c906db11932adb044f3fc9c261
eric <ericchiang1121@gmail.com>
2017-09-16 14:52:24 -0400

    Initialize bracket to top32/64/96 to start simulation

--- util/tournament_bracket.py
+++ util/tournament_bracket.py
@@ -6,12 +6,20 @@ placing-to-points conversion into an output file.
 """
 from __future__ import print_function
 import pysmash
+import pprint
+
+
 
 smash = pysmash.SmashGG()
-def test():
-    tournament = smash.tournament_show_with_brackets("get-on-my-level-2016",
-                                                     "melee-singles")
-    print(tournament)
+def init_sim_bracket():
+    pp = pprint.PrettyPrinter(indent=4)
+    brackets = smash.tournament_show_event_brackets("get-on-my-level-2016", "melee-singles")
+    #pp.pprint(brackets)
+    bracket_players = smash.bracket_show_players(brackets['bracket_ids'[-1]])  # <- bracket_id
+    # bracket_players = smash.bracket_show_players(224997) # <- if you know the id before hand
+    #pp.pprint(bracket_players)
+    return len(bracket_players)
+
 
 if __name__ == "__main__":
-    test()
+    print(init_sim_bracket())
