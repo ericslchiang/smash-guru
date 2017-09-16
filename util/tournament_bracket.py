e635b242ebec7b226c72ee9dee28be80ae92ff34
eric <ericchiang1121@gmail.com>
2017-09-16 15:04:41 -0400

    Gives the number of players in selected tournmaent bracket

--- util/tournament_bracket.py
+++ util/tournament_bracket.py
@@ -1,8 +1,7 @@
 """
-Author: austinscchiang
-Description: Given raw parsed data, parse the file for a placing-to-fantasy-
-points key-value pair. Collect all key-value pairs and stream the complete
-placing-to-points conversion into an output file.
+Author: ericslchiang
+Description: Pulls tournament brackets from smash.gg, and return the number of 
+players in the simulation bracket. 
 """
 from __future__ import print_function
 import pysmash
@@ -13,11 +12,9 @@ import pprint
 smash = pysmash.SmashGG()
 def init_sim_bracket():
     pp = pprint.PrettyPrinter(indent=4)
-    brackets = smash.tournament_show_event_brackets("get-on-my-level-2016", "melee-singles")
-    #pp.pprint(brackets)
-    bracket_players = smash.bracket_show_players(brackets['bracket_ids'[-1]])  # <- bracket_id
-    # bracket_players = smash.bracket_show_players(224997) # <- if you know the id before hand
-    #pp.pprint(bracket_players)
+    brackets = smash.tournament_show_event_brackets("get-on-my-level-2016",
+    												"melee-singles")
+    bracket_players = smash.bracket_show_players(brackets['bracket_ids'[-1]])
     return len(bracket_players)
 
 
