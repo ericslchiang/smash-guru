921fbba3140c76aeaefbf5f900e72709087d89ee
eric <ericchiang1121@gmail.com>
2017-09-16 22:14:44 -0400

    fixed styling

--- util/tournament_bracket.py
+++ util/tournament_bracket.py
@@ -20,19 +20,19 @@ class TreeNode(object):
 
 def knapSack(budget, cost, val):
     n = len(val)
-    K = [[0 for x in range(budget+1)] for x in range(n+1)]
+    dp= [[0 for x in range(budget+1)] for x in range(n+1)]
  
     # Build table K[][] in bottom up manner
     for i in range(n+1):
         for w in range(budget+1):
             if i==0 or w==0:
-                K[i][w] = 0
+                dp[i][w] = 0
             elif cost[i-1] <= w:
-                K[i][w] = max(val[i-1] + K[i-1][w-cost[i-1]],  K[i-1][w])
+                dp[i][w] = max(val[i-1] + dp[i-1][w-cost[i-1]],  dp[i-1][w])
             else:
-                K[i][w] = K[i-1][w]
+                dp[i][w] = dp[i-1][w]
  
-    return K[n][budget]
+    return dp[n][budget]
  
 def traverse(rootnode):
   thislevel = [rootnode]
@@ -99,4 +99,3 @@ if __name__ == "__main__":
 
     print(knapSack(budget, cost, val))
     """
-    
