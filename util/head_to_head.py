fe8ccc92f109bfbbca42fc16ccacfa582ca1b92f
Austin Chiang <austinscchiang@gmail.com>
2017-09-16 22:24:05 -0400

    Added historical head to head data

---
+++ util/head_to_head.py
@@ -0,0 +1,23 @@
+def head_to_head_read(input_file):
+    tag_rank = {}
+    head_to_head = None
+    with open(input_file, "rb") as f:
+        for i, line in enumerate(f):
+            tokens = line.split("|")
+            if i == 0:
+                tag_rank = {tag.lower(): (j+1) for j, tag in enumerate(tokens)}
+                num_players = len(tokens)
+                head_to_head = [[0 for i in xrange(num_players)]
+                               for i in xrange(num_players)]
+            else:
+                player_1 = i - 1
+                for player_2, token in enumerate(tokens):
+                    print player_2
+                    if token.strip():
+                        head_to_head[player_1][player_2] = int(token)
+        return (tag_rank, head_to_head)
+
+if __name__ == "__main__":
+    tag_rank, head_to_head = head_to_head_read("head_to_head.txt")
+    print tag_rank
+    print head_to_head
